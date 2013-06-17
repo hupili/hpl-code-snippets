1edf35259c3c269d78fb56af1488ca64edae1b54
HU Pili <hpl1989@gmail.com>
2013-06-17 11:46:29 +0800

    test the truncate of liquid

---
+++ ruby/test_liquid.rb
@@ -0,0 +1,37 @@
+gem 'liquid', '=2.5.0'
+require 'Liquid.rb'
+body=<<eos
+Truncate {{text | truncate: 5}}
+eos
+@template = Liquid::Template.parse(body)
+puts @template.render("text" => "I'm testing Liquid")
+puts @template.render("text" => "测试测试测试测试")
+
+body=<<eos
+Truncate {{text | truncatewords: 5}}
+eos
+@template = Liquid::Template.parse(body)
+puts @template.render("text" => "I'm testing the word truncation of Liquid")
+puts @template.render("text" => "测试测试测试测试")
+
+# Test on liquid (2.3.0)
+# Truncate I'...
+# Truncate 测试...
+# Truncate I'm testing the word truncation...
+# Truncate 测试测试测试测试
+# [ok]
+#
+# Test on liquid (2.5.0)
+# Truncate I'...
+# Truncate 测试...
+# Truncate I'm testing the word truncation...
+# Truncate 测试测试测试测试
+# [ok]
+
+exit
+# Init test: an executable minimum example 
+body=<<eos
+hi {{text}}
+eos
+@template = Liquid::Template.parse(body)
+puts @template.render("text" => "I'm testing Liquid")
