450e26cc08d142294877868014dacacffdf44eda
HU Pili <hpl1989@gmail.com>
2013-08-01 23:45:43 +0800

    rename

--- mac/dyld/inject-sys/bat.sh
+++ mac/dyld/inject-sys/bat.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-gcc -O0 -flat_namespace -dynamiclib -o openhook.dylib openhook.c
+gcc -O0 -flat_namespace -dynamiclib -o hook.dylib hook.c
 gcc -O0 -o main main.c
 ./main
-DYLD_FORCE_FLAT_NAMESPACE=1 DYLD_INSERT_LIBRARIES=openhook.dylib ./main
+DYLD_FORCE_FLAT_NAMESPACE=1 DYLD_INSERT_LIBRARIES=hook.dylib ./main
