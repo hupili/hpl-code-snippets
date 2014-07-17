ea7f634dc15785f877e8363d42df5809264ca6d8
HU Pili <hpl1989@gmail.com>
2014-07-17 12:46:21 +0800

    java snippet to test version

---
+++ java/HelloWorld.java
@@ -0,0 +1,12 @@
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+        System.out.println(System.getProperty("java.vendor"));
+        System.out.println(System.getProperty("java.vendor.url"));
+        System.out.println(System.getProperty("java.version"));
+    }
+
+}
+
---
+++ java/README.md
@@ -0,0 +1,12 @@
+HelloWorld to test java version and vendor.
+
+```
+%javac HelloWorld.java
+%java HelloWorld
+Hello, World
+Oracle Corporation
+http://java.oracle.com/
+1.8.0_05
+```
+
+Why don't they put this information in `java -version` directly?
